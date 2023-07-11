882d9e803a4f2f0a00a317a0cdb3da6a2f73aaf1
Henry Oke <henryoke@LMG-00187.local>
2023-07-11 13:43:00 -0400

    Feat: Hero Update

--- components/About.tsx
+++ components/About.tsx
@@ -7,22 +7,10 @@ export default function About() {
         About
       </h3>
       <p className="mb-4 font-light  text-[#D0CFCF] leading-7">
-        {`Recently, I've been working at a Media company, collaborating closely
-        with science writers. Together, we've been building cutting-edge
-        websites using technologies like React, TypeScript, and Node. It's been
-        an exciting journey, combining my passion for development with the
-        opportunity to contribute to the world of science communication.
-        Recently, I've been working at a Media company, collaborating closely
-        with science writers.`}
+        {`Hello! I'm a Toronto-based front-end developer with 4+ years of experience in the Media/Advertising and eCommerce industries. I specialize in creating exceptional web experiences using JavaScript, TypeScript, HTML5, CSS3, Node, and Python. With a focus on user-experice and performance, I develop visually appealing and accessible websites that engage and inform users.`}
       </p>
-      <p className="mb-4 font-light text-[#D0CFCF] leading-7">
-        {`Recently, I've been working at a Media company, collaborating closely
-        with science writers. Together, we've been building cutting-edge
-        websites using technologies like React,`}
-      </p>
-      <p className="mb-4 font-light text-[#D0CFCF] leading-7">
-        {`Hey there! I'm a friendly front-end developer based in Toronto, Canada,
-        with experience in the Media / Advertising and ecommerce industries`}
+      <p className="mb-4 font-light  text-[#D0CFCF] leading-7">
+        {`Collaborating closely with writers and e-commerce merchants has allowed me to leverage and passion for development to build solutions and experiences to meet business goals. I bring a results-oriented approach to my work, striving to deliver seamless and intuitive user experiences. I am currently seeking full-time frontend software development and full-stack roles where I can leverage my expertise to drive innovation and make a meaningful impact. Let's connect and explore how we can work together!`}
       </p>
     </div>
   );
