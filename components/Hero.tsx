2d5e6340e6ccc11e0ea23712a9179a3ef7ccc684
Henry Oke <henryoke@LMG-00187.local>
2023-09-04 17:26:26 -0400

    Feat: update links target

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,24 +1,33 @@
-import { BsLinkedin, BsGithub, BsTwitter, BsInstagram } from "react-icons/bs";
+import Link from 'next/link';
+import { BsLinkedin, BsGithub, BsTwitter, BsInstagram } from 'react-icons/bs';
 
 export default function Hero() {
   return (
-    <div className="mt-32 mb-24">
-      <h1 className="text-[29px] md:text-5xl font-bold mb-2">
+    <div className='mt-32 mb-24'>
+      <h1 className='text-[29px] md:text-5xl font-bold mb-2'>
         Hello, Im Henry Oke 👋🏾
       </h1>
-      <p className="mb-8 text-[#D0CFCF] font-light max-w-sm">
-        {` I'm a${" "}`}
-        <span className="font-bold text-white">
+      <p className='mb-8 text-[#D0CFCF] font-light max-w-sm'>
+        {` I'm a${' '}`}
+        <span className='font-bold text-white'>
           Front-end Web Developer
-        </span>{" "}
+        </span>{' '}
         {`dedicated to creating inclusive digital experiences that are accessible
         to all.`}
       </p>
-      <div className="flex gap-8">
-        <BsLinkedin size={20} />
-        <BsGithub size={20} />
-        <BsTwitter size={20} />
-        <BsInstagram size={20} />
+      <div className='flex gap-8'>
+        <Link href={'https://www.linkedin.com/in/henry-oke/'} target='_blank'>
+          <BsLinkedin size={20} />
+        </Link>
+        <Link href={'https://github.com/ok3dotdev'} target='_blank'>
+          <BsGithub size={20} />
+        </Link>
+        <Link href={'https://twitter.com/okedotdev'} target='_blank'>
+          <BsTwitter size={20} />
+        </Link>
+        <Link href={'https://www.instagram.com/itisihenry/'} target='_blank'>
+          <BsInstagram size={20} />
+        </Link>
       </div>
     </div>
   );
