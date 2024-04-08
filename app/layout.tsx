f4f011dc620053fe9e470d06925c8d69035df19c
Henry Oke <henryoke@LMG-00187.local>
2024-04-07 20:54:28 -0400

    Feat: Updates to Meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 import GoogleAnaltytics from './GoogleAnaltytics';
 
 export const metadata = {
-  title: 'Henry Oke',
+  title: 'Henry Oke - My personal site.',
   description: 'Front-end Software Developer',
 };
 
